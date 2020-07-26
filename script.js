b76e6fa6ecaa2644160e039d012db408d57d739c
ruslan <admin94.tm@mail.ru>
2020-07-26 22:35:40 +0300

    add searth

--- script.js
+++ script.js
@@ -7,4 +7,29 @@ inputDateDepart = document.querySelector('.input__date-depart'),
 cipestTicket = document.getElementById('cheapest-ticket'),
 otherCheapTickets=document.getElementById('other-cheap-tickets');
 
-const city = [];
+const city = ['Москва', 'Санкт-Петербург', 'Керч', 'Махачкала', 'Смоленск', 'Туркменабад', 'Чарджов', 'Мухасранск', 'Стамбул', 'Константинополь', 'Лондон'];
+
+
+inputCitiesFrom.addEventListener('input', ()=>{
+    dropdownCitiesFrom.textContent = '';
+  const filterCity = city.filter((item)=>{
+   const fixItem = item.toLowerCase();
+   return fixItem.includes(inputCitiesFrom.value.toLowerCase())
+    
+  })
+ filterCity.forEach(item=>{
+     const li = document.createElement('li');
+     li.classList.add('dropdown__city');
+     li.textContent=item
+     dropdownCitiesFrom.append(li)
+     console.log(li);
+ })
+
+
+
+})
+
+
+const get = (name)=>{
+console.log('Get-get :'+name);
+}

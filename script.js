294ed767d81598c66a2e0ca6e345b6faa115b4a6
ruslan <admin94.tm@mail.ru>
2020-07-28 23:54:11 +0300

    hello begin day 3 half day

--- script.js
+++ script.js
@@ -12,15 +12,17 @@ inputDateDepart = document.querySelector('.input__date-depart'),
 cipestTicket = document.getElementById('cheapest-ticket'),
 otherCheapTickets=document.getElementById('other-cheap-tickets');
 
-const city = ['Москва', 'Санкт-Петербург', 'Керч', 'Махачкала', 'Смоленск', 'Туркменабад', 'Чарджов', 'Мухасранск', 'Стамбул', 'Константинополь', 'Лондон'];
+// let city = ['Москва', 'Санкт-Петербург', 'Керч', 'Махачкала', 'Смоленск', 'Туркменабад', 'Чарджов', 'Мухасранск', 'Стамбул', 'Константинополь', 'Лондон'];
+let city=[]
 
 
-
+const API_KEY='fb844ccadfe1284ee8204a02b66fa38b';
 const citesApi='http://api.travelpayouts.com/data/ru/cities.json';
 const proxy ='https://cors-anywhere.herokuapp.com/';
+const calendar='http://min-prices.aviasales.ru/calendar_preload';
 // get data 
 //firlter
-let citi=[];
+
 
 const getDate = (url, callBack)=>{
     const request  = new XMLHttpRequest();
@@ -46,16 +48,19 @@ const showCity= (input, list)=>{
    list.textContent = '';
     if(input.value){
         const filterCity = city.filter((item)=>{
-       const fixItem = item.toLowerCase();
-     return fixItem.includes(input.value.toLowerCase())
+          if(item.name){
+            const fixItem = item.name.toLowerCase();
+            return fixItem.includes(input.value.toLowerCase())
+          }
+    
     })
     //create li elemend and show 
  filterCity.forEach(item=>{
     const li = document.createElement('li');
     li.classList.add('dropdown__city');
-    li.textContent=item
+    li.textContent=item.name
    list.append(li)
-    console.log(li);
+   
 })
 }
 }
@@ -84,13 +89,33 @@ dropdownCitiesTo.addEventListener('click', event =>{
     selectCity(event, inputCitiesTo, dropdownCitiesTo)
  } )
 
+getDate(proxy+citesApi, (data)=>{
+    city = JSON.parse(data).filter(item=> item.name)
+    console.log(city);
+})
 
- getDate(proxy + citesApi, (data)=>{
 
 
-    const dataCiies =JSON.parse(data);
-    citi =dataCiies.filter(item=>{
-        return true;
-    })
-     console.log(citi);
- })
+
+formSearch.addEventListener('submit', event=>{
+    event.preventDefault();
+    const formData={
+        form:city.find((item)=> inputCitiesFrom.value ===item.name).code,
+        to:city.find((item)=> inputCitiesTo.value ===item.name).code,
+        when:inputDateDepart.value
+    }
+    console.log(formData);
+})
+
+
+
+//get all city from 
+//  getDate(proxy + calendar+ '?depart_date=2020-05-25&origin=SVX&destination=KGD&one_way=true&token='+API_KEY, (data)=>{
+
+
+//     city =JSON.parse(data).filter((item)=>{
+//         return item.name;
+//     })
+  
+
+//  })
